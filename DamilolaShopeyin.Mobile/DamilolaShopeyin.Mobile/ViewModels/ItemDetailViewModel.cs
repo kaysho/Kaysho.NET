856df75e690feeba64ec2bc14b03088b90010f96
Shopeyin Damilola <kaysho109@gmail.com>
2019-10-17 20:03:06 +0100

    Add project files.

---
+++ DamilolaShopeyin.Mobile/DamilolaShopeyin.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DamilolaShopeyin.Core.Models;
+
+namespace DamilolaShopeyin.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ DamilolaShopeyin.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DamilolaShopeyin.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
