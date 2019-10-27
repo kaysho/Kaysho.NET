8dcc6512547d4eb636a69412bb29fce2a154bab3
Shopeyin Damilola <kaysho109@gmail.com>
2019-10-27 22:31:57 +0100

    Rename the mobile app and blazor app to Kaysho.NET

---
+++ Kaysho.NET.Mobile/Kaysho.NET.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Kaysho.NET.Mobile.Models;
+
+namespace Kaysho.NET.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
